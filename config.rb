abe12443f2698ee7fa03e21b3681246e2988f3d9
kashiro <ksrtmyk@gmail.com>
2013-07-13 12:10:47 +0900

    update settings

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ sass_dir = "dev/src/assets/styles/sass"
 css_dir = "dev/src/assets/styles/css"
 images_dir = "dev/src/assets/images"
 
-sass_options = { :debug_info => true }
+# sass_options = { :debug_info => true }
 relative_assets = true
 output_style = :compressed
 environment = :production
